b11ab930f7c90d99f196dd3079354f8696e144a1
Luis Salcedo Salas <rideluis@gmail.com>
2022-05-10 16:09:10 -0500

    feat: add testing file

---
+++ app.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
+
+describe('', () => {
+    beforeEach(() => {
+        
+    })
+})
